dd27e9d01434793e97bf684906ac92c96c263e8d
jws-page <g1050307@gm.pu.edu.tw>
2021-04-23 13:43:57 +0800

    upd demo ver2: add gotop

--- js/main.js
+++ js/main.js
@@ -73,3 +73,17 @@
     }, 1000);
 }
 
+window.onscroll = scrollFunction;
+
+function scrollFunction() { 
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("top").style.display = "block";
+  } else {
+    document.getElementById("top").style.display = "none";
+  }
+}
+
+
+function goTop() {
+  document.documentElement.scrollTop = 0;
+}
